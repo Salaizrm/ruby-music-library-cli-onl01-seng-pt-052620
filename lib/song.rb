6e6692208b4fae1c7b93a3ae7dbfb01db44210ae
Robert Salaiz <work.salaizrm@gmail.com>
2020-07-07 00:21:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,19 @@ class Song
 
   def initialize(name)
     @name = name
+    save
   end
 
+  def self.all
+    @@all
+  end
 
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
 
 end
