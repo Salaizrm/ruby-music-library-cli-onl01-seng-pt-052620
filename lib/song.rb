01d002d5a2010b99762d3487d3c0037ff5e502e9
Robert Salaiz <work.salaizrm@gmail.com>
2020-07-07 00:52:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,8 +41,14 @@ class Song
     all.find {|s| s.name == name }
   end
 
-  def self.find_or_create_by_name(name)
+  def self.find_or_create_by_name(filename)
+    parts = filename.split(" - ")
+    artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3", "")
 
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+
+    new(song_name, artist, genre)
   end
 
 end
